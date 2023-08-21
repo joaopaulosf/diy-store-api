c7c4ba1d9791a43bd39daefdba18db9ded25671e
joaopaulosf <jpforcode@gmail.com>
2023-08-21 13:08:03 -0300

    feat: Add root page

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Routes
+const root = require("./routes/root");
+const invalid = require("./routes/invalid");
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", root);
+app.all("*", invalid);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
