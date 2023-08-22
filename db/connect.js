355bfdd7c8b8f0fdd479656a284f3f0f5c181135
joaopaulosf <jpforcode@gmail.com>
2023-08-21 22:07:29 -0300

    feat: Connect to db

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = connect;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./db/connect");
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 // Routes
 const root = require("./routes/root");
 const invalid = require("./routes/invalid");
+const product = require("./routes/product");
 
 app.use(cors());
 
@@ -16,6 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", root);
+app.use("/", root);
+app.use("/products", product);
+
+// last
 app.all("*", invalid);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+});
