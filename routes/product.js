7ec017f9fa2341be3500edb827bcf915241f9af3
joaopaulosf <jpforcode@gmail.com>
2023-08-23 21:53:52 -0300

    feat: add category route

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { addProduct, getAllProducts } = require("../controllers/product");
+const {
+  addProduct,
+  getAllProducts,
+  getProduct,
+  getCategories,
+  getByCategory,
+} = require("../controllers/product");
 
 router.route("/").get(getAllProducts).post(addProduct);
+router.get("/categories", getCategories);
+router.get("/category/:category", getByCategory);
+router.get("/:productId", getProduct);
 
 module.exports = router;
